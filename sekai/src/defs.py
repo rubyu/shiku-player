6f270d76619eeec502eff57a2ef41dba9d774d6e
rubyu <ruby.u.g@gmail.com>
2011-09-21 17:09:49 +0900

    パーサをモジュールに移動。

--- sekai/src/defs.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf-8 -*-
-
-__author__="rubyu"
-__date__ ="$2011/09/21 14:07:07$"
-
-VOICE_FILE = "/home/rubyu/Documents/voice.bin"
-SCRIPT_FILE = "World.hcb"
---
+++ sekai/src/sekai/defs.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+
+__author__="rubyu"
+__date__ ="$2011/09/21 14:07:07$"
+
+VOICE_FILE = "/home/rubyu/Documents/sekai/voice.bin"
+SCRIPT_FILE = "/home/rubyu/Documents/sekai/World.hcb"
